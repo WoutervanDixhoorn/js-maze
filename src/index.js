5433969a7983393d3daa068e7bb0c8d02ec09479
Wouter <woutervdixhoorn@gmail.com>
2019-09-24 19:06:41 +0200

    Finished basic maze generation algorithm

--- src/index.js
+++ src/index.js
@@ -7,12 +7,22 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 let cols, rows;
-var size = 50;
+var size = 40;
 var grid = [];
+var current;
+
+var stack = [];
+
+//FPS
+var fps = 1000;
+var now;
+var then = Date.now();
+var interval = 1000/fps;
+var delta;
 
 //Initialize variables, etc.
 function init(){
-    cols = Math.floor((innerHeight/2)/50);
+    cols = Math.floor(innerHeight/size);
     rows = cols;
 
     for(j = 0; j < rows; j++){
@@ -21,33 +31,146 @@ function init(){
             grid.push(cell);
         }
     }
+
+    current = grid[0];
 }
 //run init
 init();
 //////////
 
-//Cell
+//#region MY FUNCTIONS
 function Cell(i,j){
     this.i = i;
     this.j = j;
-    
+    this.walls = [true,true,true,true];
+    this.visited = false;
+
     this.show = function (){
-        var offsetx = (window.innerWidth/2 - (cols*50)/2);
-        var offsety = window.innerHeight/2 - (cols*50)/2;
-        var x = (this.i*size) + offsetx;
-        var y = (this.j*size) + offsety;
-        c.fillStyle = 'rgba(255,255,255,1)';
-        c.strokeStyle = 'rgba(0,0,0,1)';
-        c.strokeRect(x,y,size,size);
-        c.fillRect(x,y,size,size);
+        var offsetx = (window.innerWidth/2 - (cols*size)/2);
+        var offsety = window.innerHeight/2 - (cols*size)/2;
+        var x = Math.floor((this.i*size) + offsetx);
+        var y = Math.floor((this.j*size) + offsety);
+
+        if(this.walls[0]){
+            drawLine(x,y,x+size,y,'#FFFFFF');
+        }
+        if(this.walls[1]){
+            drawLine(x+size,y,x+size,y+size,'#FFFFFF');
+        }
+        if(this.walls[2]){
+            drawLine(x+size,y+size,x,y+size,'#FFFFFF');
+        }
+        if(this.walls[3]){
+            drawLine(x,y+size,x,y,'#FFFFFF');
+        }
+
+        if(this.visited){
+            drawRect(x,y,size,'rgba(255,0,255,0.6)');
+        } 
+        if(current == this) {
+            drawRect(x,y,size,'rgba(255,255,255,0.5)');
+        }
+    }
+
+    this.checkNeighbours = function (){
+        
+
+        var neighbours = [];
+
+        var top = grid[index(i,j-1)];
+        var right = grid[index(i+1,j)];
+        var bottom = grid[index(i,j+1)];
+        var left = grid[index(i-1,j)];
+
+        if(top && !top.visited){
+            neighbours.push(top);
+        }
+        if(right && !right.visited){
+            neighbours.push(right);
+        }
+        if(bottom && !bottom.visited){
+            neighbours.push(bottom);
+        }
+        if(left && !left.visited){
+            neighbours.push(left);
+        }
+
+
+        if(neighbours.length > 0){
+            var r = Math.floor(Math.random() * neighbours.length);
+            return neighbours[r];
+        }else{
+            return undefined;
+        }
     }
 }
 
+function drawLine(x1,y1,x2,y2,color){
+    c.beginPath();
+    c.strokeStyle = color;
+    c.moveTo(x1,y1);
+    c.lineTo(x2,y2);
+    c.lineWidth = 2;
+    c.stroke();
+    c.closePath();
+}
+
+function drawRect(x,y,size,color){
+    c.fillStyle = color;
+    c.fillRect(x,y,size,size);
+}
+
+function removeWalls(a,b){
+
+    var x = a.i - b.i;
+    if(x == 1){
+        a.walls[3] = false;
+        b.walls[1] = false;
+    }else if(x == -1){
+        a.walls[1] = false;
+        b.walls[3] = false;
+    }
+
+    var y = a.j - b.j;
+    if(y == 1){
+        a.walls[0] = false;
+        b.walls[2] = false;
+    }else if(y == -1){
+        a.walls[2] = false;
+        b.walls[0] = false;
+    }
+}
+
+function index(i,j){
+    if(i < 0 || j < 0 || i > cols-1 || j > rows - 1){
+        return -1;
+    }
+    return i + j * cols;
+}
+
+//#endregion
+
 //Rendering on screen
 function draw(){
+    c.clearRect(0,0,window.innerWidth,window.innerHeight);
+
     grid.forEach(function (element){
         element.show();
     });
+
+    current.visited = true;
+    var next = current.checkNeighbours();
+    if(next){
+        next.visited = true;
+
+        stack.push(current);
+        
+        removeWalls(current,next);
+
+        current = next;
+    }else if(stack.length > 0){
+        current = stack.pop();
+    }
 }
 
 //Logic in here
@@ -57,9 +180,18 @@ function update(){
 
 function loop(){
     window.requestAnimationFrame(loop);
-    //Draw,Update
-    draw();
-    update();
+    // if enough time has elapsed, draw the next frame
+
+    now = Date.now();
+    delta = now - then;
+    if (delta > interval) {
+        then = now - (delta % interval);
+        draw();
+        update();
+    }
 }
-window.requestAnimationFrame(loop);
 loop();
+
+
+
+
